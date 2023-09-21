e11831dc9ce093a0a2f44193f8a376a300e1fccc
Sai Sunhith Reddy Nagireddy <sxn220005@utdallas.edu>
2023-09-21 15:58:43 -0500

    Initial version of chemicalsearch generated by JHipster-6.4.0

---
+++ src/main/java/com/application/chemicalsearch/config/FeignConfiguration.java
@@ -0,0 +1,22 @@
+package com.application.chemicalsearch.config;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClientsConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@EnableFeignClients(basePackages = "com.application.chemicalsearch")
+@Import(FeignClientsConfiguration.class)
+public class FeignConfiguration {
+
+    /**
+     * Set the Feign specific log level to log client REST requests.
+     */
+    @Bean
+    feign.Logger.Level feignLoggerLevel() {
+        return feign.Logger.Level.BASIC;
+    }
+
+}
